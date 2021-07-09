fbb661d7b7bbf1bc13b6a4dc3e70f2d585110ea8
Leandro P. Gomes <grupolpgomes@gmail.com>
2021-07-09 09:40:17 -0300

    add testes and update category

--- app/controllers/admin/v1/categories_controller.rb
+++ app/controllers/admin/v1/categories_controller.rb
@@ -1,33 +1,36 @@
 module Admin::V1
     class CategoriesController < ApiController
        def index
-         @categories = Category.all
+          @categories = Category.all
        end 
 
        def create
-        @category = Category.new
-        @category.attributes = category_params
+          @category = Category.new
+          @category.attributes = category_params
        
-        save_category!
+          save_category!
        end
 
+       def update
+        @category = Category.find(params[:id])
+        @category.attributes = category_params
+        save_category!
+       end
 
        private 
 
        def category_params
-        return {} unless params.has_key?(:category)
-        params.require(:category).permit(:name)  
+          return {} unless params.has_key?(:category)
+          params.require(:category).permit(:name)  
        end
        
        def save_category!
-        @category.save!
-        render :show
+          @category.save!
+          render :show
        rescue 
-        render_error(fields: @category.errors.messages)
+          render_error(fields: @category.errors.messages)
         
        end
   
-
-
     end
 end
